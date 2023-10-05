2842b89ab0b85fb7c3bab70365c605f6aad29d48
biopnk <moldychewbacca@googlemail.com>
2023-10-05 19:45:55 +0100

    Change to struct version

--- main.c
+++ main.c
@@ -3,7 +3,19 @@
 #include "raylib.h"
 #include "raymath.h"
 
-Vector2 checkMovement(Vector2 position, int sprite_height, int sprite_width, float speed);
+typedef struct sprite
+{
+    Texture2D sprite;
+    Vector2 pos;
+    int sprite_height;
+    int sprite_width;
+    Vector2 min;
+    Vector2 max;
+    Vector2 clamp;
+} sp;
+
+Vector2 checkMovement(sp sprite, float speed);
+sp spriteClamp(sp sprite);
 // Vector2 followerMovement(Vector2 follower_position, Vector2 position);
 
 int width = 800;
@@ -13,24 +25,31 @@ int scale = 4;
 
 int main(void)
 {
-    Texture2D sprite;
-    Texture2D follower;
+    sp sprite;
+    sp follower;
+
+    sprite.sprite = LoadTexture("tile_0121.png");
+    sprite.pos = (Vector2){0, 10};
+    sprite.sprite_height = sprite.sprite.height * scale;
+    sprite.sprite_width = sprite.sprite.height * scale;
+    
+    spriteClamp(sprite);
+
+    follower.sprite = LoadTexture("tile_0123.png");
+    follower.sprite_height = sprite.sprite.height * scale;
+    follower.sprite_width = sprite.sprite.width * scale;
+    follower.pos = (Vector2){width - follower.sprite_width, 10};
+    spriteClamp(follower);
+
     Sound sound;
     // Music music;
 
     InitWindow(width, height, "title");
     InitAudioDevice();
 
-    sprite = LoadTexture("tile_0121.png");
-    follower = LoadTexture("tile_0123.png");
     sound = LoadSound("star.wav");
     // music = LoadMusicStream("dirediredocks.mp3");
     // PlayMusicStream(music);
-
-    Vector2 pos = {0, 10};
-    Vector2 follower_pos = {width - (follower.width * scale), 10};
-    int sprite_width = sprite.width * scale;
-    int sprite_height = sprite.height * scale;
 
     while (!WindowShouldClose())
     {
@@ -38,14 +57,14 @@ int main(void)
 
         float speed = GetFrameTime() * velocity;
 
-        pos = checkMovement(pos, sprite_height, sprite_width, speed);
+        sprite.pos = checkMovement(sprite, speed);
         // follower_pos = followerMovement(follower_pos, pos);
 
         Rectangle spriteRect = {
-            pos.x,
-            pos.y,
-            sprite.width * scale,
-            sprite.height * scale
+            sprite.pos.x,
+            sprite.pos.y,
+            sprite.sprite_width,
+            sprite.sprite_height
         };
 
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
@@ -58,8 +77,8 @@ int main(void)
 
         BeginDrawing();
             ClearBackground(GRAY);
-            DrawTextureEx(sprite, pos, 0, scale, RED);
-            DrawTextureEx(follower, follower_pos, 0, scale, GRAY);
+            DrawTextureEx(sprite.sprite, sprite.pos, 0, scale, RED);
+            DrawTextureEx(follower.sprite, follower.pos, 0, scale, GRAY);
         EndDrawing();
     }
     // StopMusicStream(music);
@@ -71,22 +90,26 @@ int main(void)
     return 0;
 }
 
-Vector2 checkMovement(Vector2 position, int sprite_height, int sprite_width, float speed)
+Vector2 checkMovement(sp sprite, float speed)
 {
-    Vector2 min = {0, 0};
-    Vector2 max = {width - sprite_width, height - sprite_height};
 
-    Vector2 clamp = Vector2Clamp(position, min, max);
+    if (IsKeyDown(KEY_D) && sprite.clamp.x != sprite.max.x)
+        sprite.pos.x += speed;
+    if (IsKeyDown(KEY_A) && sprite.clamp.x != sprite.min.x)
+        sprite.pos.x -= speed;
+    if (IsKeyDown(KEY_W) && sprite.clamp.y != sprite.min.y)
+        sprite.pos.y -= speed;
+    if (IsKeyDown(KEY_S) && sprite.clamp.y != sprite.max.y)
+        sprite.pos.y += speed;
 
-    if (IsKeyDown(KEY_D) && clamp.x != max.x)
-        position.x += speed;
-    if (IsKeyDown(KEY_A) && clamp.x != min.x)
-        position.x -= speed;
-    if (IsKeyDown(KEY_W) && clamp.y != min.y)
-        position.y -= speed;
-    if (IsKeyDown(KEY_S) && clamp.y != max.y)
-        position.y += speed;
+    printf("%f, %f\n", sprite.clamp.x, sprite.clamp.y);
+    return sprite.pos;
+}
 
-    printf("%f, %f\n", clamp.x, clamp.y);
-    return position;
+sp spriteClamp(sp sprite)
+{
+    sprite.min = (Vector2){0, 0};
+    sprite.max = (Vector2){width - sprite.sprite_width, height - sprite.sprite_height};
+
+    sprite.clamp = Vector2Clamp(sprite.pos, sprite.min, sprite.max);
 }
