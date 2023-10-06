8a7f287211e4ab6003ac9a82f5bce0801807069f
biopnk <moldychewbacca@googlemail.com>
2023-10-06 06:03:50 +0100

    Create follow mechanic, abstract npc into struct After some searching, the npc is given a point to hit, and follows this at a given speed until reached. Speed should be lower than player speed so they can get away.

--- main.c
+++ main.c
@@ -15,14 +15,23 @@ typedef struct sprite
     Vector2 clamp;
 } sp;
 
+typedef struct npc
+{
+    struct sprite sp;
+    float base_distance;
+    Vector2 target_location;
+} npc;
+
+float sign(float z);
 sp checkMovement(sp *sprite, float speed);
 sp spriteClamp(sp *sprite);
-// sp followerMovement(Vector2 follower_position, Vector2 position);
+npc followerMovement(npc *npc, sp *target, float speed);
 
 int width = 800;
 int height = 450;
 float velocity = 200;
 int scale = 4;
+float distance = 150;
 
 int main(void)
 {    
@@ -41,19 +50,19 @@ int main(void)
     // PlayMusicStream(music);
 
     sp sprite;
-    sp follower;
+    npc follower;
 
     sprite.sprite = LoadTexture("tile_0121.png");
     sprite.pos = (Vector2){0, 10};
     sprite.sprite_height = sprite.sprite.height * scale;
     sprite.sprite_width = sprite.sprite.height * scale;
     
-    printf("clamp: %f, %f\nmax: %f, %f\nmin: %f, %f\n", sprite.clamp.x, sprite.clamp.y, sprite.max.x, sprite.max.y, sprite.min.x, sprite.min.y);
+    // printf("clamp: %f, %f\nmax: %f, %f\nmin: %f, %f\n", sprite.clamp.x, sprite.clamp.y, sprite.max.x, sprite.max.y, sprite.min.x, sprite.min.y);
 
-    follower.sprite = LoadTexture("tile_0123.png");
-    follower.sprite_height = sprite.sprite.height * scale;
-    follower.sprite_width = sprite.sprite.width * scale;
-    follower.pos = (Vector2){width - follower.sprite_width, 10};
+    follower.sp.sprite = LoadTexture("tile_0123.png");
+    follower.sp.sprite_height = sprite.sprite.height * scale;
+    follower.sp.sprite_width = sprite.sprite.width * scale;
+    follower.sp.pos = (Vector2){width - follower.sp.sprite_width, 10};
 
     while (!WindowShouldClose())
     {
@@ -64,29 +73,16 @@ int main(void)
         checkMovement(&sprite, speed);
         spriteClamp(&sprite);
 
-        // follower_pos = followerMovement(follower_pos, pos);
-        spriteClamp(&follower);
-
-        Rectangle spriteRect = {
-            sprite.pos.x,
-            sprite.pos.y,
-            sprite.sprite_width,
-            sprite.sprite_height
-        };
-
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-        {
-            if (CheckCollisionPointRec(GetMousePosition(), spriteRect))
-            {
-                PlaySound(sound);
-            }
-        }
+        follower.base_distance = Vector2Distance(follower.sp.pos, sprite.pos);
+        follower.target_location = Vector2MoveTowards(sprite.pos, follower.sp.pos, 20);
+        followerMovement(&follower, &sprite, speed / 2);
+        // spriteClamp(&follower.sp);
 
         BeginDrawing();
             ClearBackground(GRAY);
             DrawFPS(10, height - 20);
             DrawTextureEx(sprite.sprite, sprite.pos, 0, scale, RED);
-            DrawTextureEx(follower.sprite, follower.pos, 0, scale, GRAY);
+            DrawTextureEx(follower.sp.sprite, follower.sp.pos, 0, scale, GRAY);
         EndDrawing();
     }
     // StopMusicStream(music);
@@ -110,10 +106,47 @@ sp checkMovement(sp *sprite, float speed)
         sprite->pos.y += speed;
 }
 
+npc followerMovement(npc *npc, sp *target, float speed)
+{
+    Vector2 move_this_frame = {0, 0};
+    Vector2 difference;
+
+    if (npc->base_distance <= distance + npc->sp.sprite_width)
+    {
+        difference = Vector2Subtract(npc->target_location, npc->sp.pos);
+        difference.x = sign(difference.x);
+        difference.y = sign(difference.y);
+
+        if (npc->base_distance != 0)
+        {
+            move_this_frame = Vector2Scale(difference, speed);
+            // move_this_frame.x = roundf(move_this_frame.x);
+            // move_this_frame.y = roundf(move_this_frame.y);
+            npc->sp.pos = Vector2Add(npc->sp.pos, move_this_frame);
+            printf("%f, %f\n", move_this_frame.x, move_this_frame.y);
+        }
+        // move_this_frame = Vector2Scale(target_location, speed);
+        // printf("move_this_frame: %f, %f\n", move_this_frame.x, move_this_frame.y);
+
+        // // sprite->pos = Vector2Subtract(target_location, move_this_frame);
+        printf("sprite: %f, %f\n", target->pos.x, target->pos.y);
+        printf("target_location: %f, %f\n", npc->target_location.x, npc->target_location.y);
+        printf("follower: %f, %f\n", npc->sp.pos.x, npc->sp.pos.y);
+    }
+
+}
+
 sp spriteClamp(sp *sprite)
 {
     sprite->min = (Vector2){0, 0};
     sprite->max = (Vector2){width - sprite->sprite_width, height - sprite->sprite_height};
 
     sprite->clamp = Vector2Clamp(sprite->pos, sprite->min, sprite->max);
+}
+
+float sign(float z)
+{
+    z = (z > 0) - (z < 0);
+
+    return z;
 }
