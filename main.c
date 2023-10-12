7eb94bf7db5cd92bf224a15e0003ef02dfd19a11
biopnk <moldychewbacca@googlemail.com>
2023-10-12 12:36:04 +0100

    Refactor code to abstract entities

--- main.c
+++ main.c
@@ -9,24 +9,42 @@ typedef struct tmr
     float lifetime;
 } tmr;
 
-typedef struct sprite
+// clamping struct
+typedef struct boundary
 {
-    Texture2D sprite;
-    Vector2 pos;
-    int sprite_height;
-    int sprite_width;
-    bool visible;
-    tmr visible_tmr;
-    float visible_lifetime;
     Vector2 min;
     Vector2 max;
     Vector2 clamp;
+} bounds;
+
+typedef struct position
+{
+    Vector2 pos;
+    int sprite_height;
+    int sprite_width;
+} pos;
+
+// invisibility struct
+typedef struct invisibility
+{
+    bool visible;
+    tmr visible_tmr;
+    float visible_lifetime;
+} invis;
+
+typedef struct sprite
+{
+    Texture2D sprite;
+    pos pos;
+    bounds bounds;
+    invis invis;
     Color colour;
 } sp;
 
 typedef struct npc
 {
-    struct sprite sp;
+    Texture2D sprite;
+    pos pos;
     float base_distance;
     Vector2 target_location;
 } npc;
@@ -36,7 +54,7 @@ void updateTimer(tmr *tmr);
 bool timerDone(tmr *tmr);
 float sign(float z);
 sp checkMovement(sp *sprite, float speed);
-sp checkVisible(sp *sprite);
+sp checkVisible(invis *invis, Color *colour);
 sp spriteClamp(sp *sprite);
 npc followerMovement(npc *npc, sp *target, float speed);
 
@@ -66,19 +84,18 @@ int main(void)
     npc follower;
 
     sprite.sprite = LoadTexture("tile_0121.png");
-    sprite.pos = (Vector2){0, 10};
-    sprite.sprite_height = sprite.sprite.height * scale;
-    sprite.sprite_width = sprite.sprite.height * scale;
-    sprite.visible = true;
+    sprite.pos.pos = (Vector2){0, 10};
+    sprite.pos.sprite_height = sprite.sprite.height * scale;
+    sprite.pos.sprite_width = sprite.sprite.height * scale;
+    sprite.invis.visible = true;
+    sprite.invis.visible_tmr = (tmr){0};
+    sprite.invis.visible_lifetime = 2;
     sprite.colour = RED;
-    sprite.visible_tmr = (tmr){0};
-    sprite.visible_lifetime = 2;
     
-    follower.sp.sprite = LoadTexture("tile_0123.png");
-    follower.sp.sprite_height = sprite.sprite.height * scale;
-    follower.sp.sprite_width = sprite.sprite.width * scale;
-    follower.sp.pos = (Vector2){width - follower.sp.sprite_width, 10};
-    follower.sp.visible = true;
+    follower.sprite = LoadTexture("tile_0123.png");
+    follower.pos.sprite_height = sprite.sprite.height * scale;
+    follower.pos.sprite_width = sprite.sprite.width * scale;
+    follower.pos.pos = (Vector2){width - follower.pos.sprite_width, 10};
 
     while (!WindowShouldClose())
     {
@@ -87,19 +104,18 @@ int main(void)
         float speed = GetFrameTime() * velocity;
 
         checkMovement(&sprite, speed);
-        checkVisible(&sprite);
+        checkVisible(&sprite.invis, &sprite.colour);
         spriteClamp(&sprite);
 
-        follower.base_distance = Vector2Distance(follower.sp.pos, sprite.pos);
-        follower.target_location = Vector2MoveTowards(sprite.pos, follower.sp.pos, 20);
+        follower.base_distance = Vector2Distance(follower.pos.pos, sprite.pos.pos);
+        follower.target_location = Vector2MoveTowards(sprite.pos.pos, follower.pos.pos, 20);
         followerMovement(&follower, &sprite, speed);
-        spriteClamp(&follower.sp);
 
         BeginDrawing();
             ClearBackground(GRAY);
             DrawFPS(10, height - 20);
-            DrawTextureEx(sprite.sprite, sprite.pos, 0, scale, sprite.colour);
-            DrawTextureEx(follower.sp.sprite, follower.sp.pos, 0, scale, GRAY);
+            DrawTextureEx(sprite.sprite, sprite.pos.pos, 0, scale, sprite.colour);
+            DrawTextureEx(follower.sprite, follower.pos.pos, 0, scale, GRAY);
         EndDrawing();
     }
     // StopMusicStream(music);
@@ -113,43 +129,43 @@ int main(void)
 
 sp checkMovement(sp *sprite, float speed)
 {
-    if (IsKeyDown(KEY_D) && sprite->clamp.x != sprite->max.x)
-        sprite->pos.x += speed;
-    if (IsKeyDown(KEY_A) && sprite->clamp.x != sprite->min.x)
-        sprite->pos.x -= speed;
-    if (IsKeyDown(KEY_W) && sprite->clamp.y != sprite->min.y)
-        sprite->pos.y -= speed;
-    if (IsKeyDown(KEY_S) && sprite->clamp.y != sprite->max.y)
-        sprite->pos.y += speed;
+    if (IsKeyDown(KEY_D) && sprite->bounds.clamp.x != sprite->bounds.max.x)
+        sprite->pos.pos.x += speed;
+    if (IsKeyDown(KEY_A) && sprite->bounds.clamp.x != sprite->bounds.min.x)
+        sprite->pos.pos.x -= speed;
+    if (IsKeyDown(KEY_W) && sprite->bounds.clamp.y != sprite->bounds.min.y)
+        sprite->pos.pos.y -= speed;
+    if (IsKeyDown(KEY_S) && sprite->bounds.clamp.y != sprite->bounds.max.y)
+        sprite->pos.pos.y += speed;
 }
 
-sp checkVisible(sp *sprite)
+sp checkVisible(invis *invis, Color *colour)
 {
     if (IsKeyPressed(KEY_SPACE))
     {
-        startTimer(&sprite->visible_tmr, sprite->visible_lifetime);
+        startTimer(&invis->visible_tmr, invis->visible_lifetime);
     }
     
     if (IsKeyDown(KEY_SPACE))
     {
-        updateTimer(&sprite->visible_tmr);
+        updateTimer(&invis->visible_tmr);
     }
     else
     {
-        sprite->visible_tmr.lifetime = 0;
+        invis->visible_tmr.lifetime = 0;
     }
 
-    printf("%f\n", sprite->visible_tmr.lifetime);
+    printf("%f\n", invis->visible_tmr.lifetime);
 
-    if (!timerDone(&sprite->visible_tmr))
+    if (!timerDone(&invis->visible_tmr))
     {
-        sprite->visible = false;
-        sprite->colour = ORANGE;
+        invis->visible = false;
+        *colour = ORANGE;
     }
     else
     {
-        sprite->visible = true;
-        sprite->colour = RED;
+        invis->visible = true;
+        *colour = RED;
     }
 }
 
@@ -158,16 +174,16 @@ npc followerMovement(npc *npc, sp *target, float speed)
     Vector2 move_this_frame = {0, 0};
     Vector2 difference;
 
-    if (npc->base_distance <= distance + npc->sp.sprite_width && target->visible)
+    if (npc->base_distance <= distance + npc->pos.sprite_width && target->invis.visible)
     {
-        difference = Vector2Subtract(npc->target_location, npc->sp.pos);
+        difference = Vector2Subtract(npc->target_location, npc->pos.pos);
         difference.x = sign(difference.x);
         difference.y = sign(difference.y);
 
         if (npc->base_distance != 0) 
         {
                 move_this_frame = Vector2Scale(difference, speed);
-                npc->sp.pos = Vector2Add(npc->sp.pos, move_this_frame);
+                npc->pos.pos = Vector2Add(npc->pos.pos, move_this_frame);
         }   
     }
 
@@ -175,10 +191,10 @@ npc followerMovement(npc *npc, sp *target, float speed)
 
 sp spriteClamp(sp *sprite)
 {
-    sprite->min = (Vector2){0, 0};
-    sprite->max = (Vector2){width - sprite->sprite_width, height - sprite->sprite_height};
+    sprite->bounds.min = (Vector2){0, 0};
+    sprite->bounds.max = (Vector2){width - sprite->pos.sprite_width, height - sprite->pos.sprite_height};
 
-    sprite->clamp = Vector2Clamp(sprite->pos, sprite->min, sprite->max);
+    sprite->bounds.clamp = Vector2Clamp(sprite->pos.pos, sprite->bounds.min, sprite->bounds.max);
 }
 
 float sign(float z)
